c6c617a2f95de86aea34ec456d91ead401e15f4e
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-21 14:03:38 +0200

    Starting point?

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -7,21 +7,50 @@ open Fable.Core.JsInterop
 open Fable.Import
 
 open Canvas
+open Physics.Consts
 
-type Model = int
+type Model =
+    { Engine : Matter.Engine
+      Player : Matter.Body
+      Balls : Matter.Body [] }
 
 type Msg =
     | Tick of delta : float
 
-let init () = 0
+let init () =
+    let engine, player, balls = Physics.init ()
+    { Engine = engine
+      Player = player
+      Balls = balls }
 
 let update (model: Model) = function
-    | Tick _ -> model
+    | Tick delta ->
+        Physics.update model.Engine delta
+        model
+
+let renderShape (ctx: Context) style (shape: Matter.Body) =
+    let vertices = shape.vertices |> Array.map (fun v -> v.x, v.y)
+    Canvas.Shape(ctx, style, vertices)
 
 let view (model : Model) (ctx: Context) _ =
+    let zoom =
+        min
+            (CANVAS_WIDTH / WORLD_WIDTH)
+            (CANVAS_HEIGHT / WORLD_HEIGHT)
+    ctx.clearRect(0., 0., CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.save()
+    // Translate to the center
+    ctx.translate(CANVAS_WIDTH / 2., CANVAS_HEIGHT / 2.)
+    // Apply zoom
+    ctx.scale(zoom, zoom)
+
+    renderShape ctx !^"yellow" model.Player
+
     ctx.restore()
 
 let subscribe (canvas: Browser.HTMLCanvasElement) dispatch (model : Model) =
-    ()
+    canvas.width <- CANVAS_WIDTH
+    canvas.height <- CANVAS_HEIGHT
+    canvas.style.background <- "black"
 
 Canvas.Start("canvas", init(), Tick, update, view, subscribe)
