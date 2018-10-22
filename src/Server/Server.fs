dd9952c35e7ad0de03a2432687dd2c9782e47cfe
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-22 15:15:24 +0200

    Add shared and server side

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -11,22 +11,90 @@ open Shared
 open Fable.Remoting.Server
 open Fable.Remoting.Giraffe
 open Microsoft.WindowsAzure.Storage
+open System
 
 let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
 let publicPath = tryGetEnv "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
 let storageAccount = tryGetEnv "STORAGE_CONNECTIONSTRING" |> Option.defaultValue "UseDevelopmentStorage=true" |> CloudStorageAccount.Parse
 let port = 8085us
 
-let getInitCounter() : Task<Counter> = task { return 42 }
+
+let blobClient = storageAccount.CreateCloudBlobClient()
+let containerName = "scores-container"
+let blobRef = blobClient.GetContainerReference containerName
+
+module Json =
+    open Fable.Remoting
+    open Newtonsoft.Json
+
+    let jsonConverter = Json.FableJsonConverter () :> JsonConverter
+    let serialize value = JsonConvert.SerializeObject(value, [|jsonConverter|])
+    let deserialize json = JsonConvert.DeserializeObject<_>(json, [|jsonConverter|])
+
+let blob =
+    task {
+        let! _ = blobRef.CreateIfNotExistsAsync ()
+        let file = blobRef.GetBlockBlobReference "scores.json"
+        let! exists = file.ExistsAsync()
+        if not exists then do! file.UploadTextAsync "[]"
+        return file
+    }
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
+let getScores () : Task<Scores> =
+    task {
+        let! json = blob.DownloadTextAsync()
+        return Json.deserialize json
+    }
+
+let leaseTime = TimeSpan.FromSeconds 15.
+
+let addScore (score : Score) =
+    task {
+        let! lease = blob.AcquireLeaseAsync(Nullable.op_Implicit leaseTime, null)
+        let! scores = getScores ()
+        let newScores = score :: scores
+        let json = Json.serialize newScores
+        let condition = AccessCondition.GenerateLeaseCondition lease
+        do! blob.UploadTextAsync(json, condition, null, null)
+        do! blob.ReleaseLeaseAsync condition
+        return newScores
+    }
+
+let highScores (scores : Scores) : Scores =
+    let sorted = List.sortByDescending snd scores
+    if sorted.Length > HighScores.limit then
+        List.take HighScores.limit sorted
+    else
+        sorted
+
+let getHighScores() : Task<Scores> =
+    task {
+        let! scores = getScores()
+        return highScores scores
+    }
+
+let submitHighScore (name, score) : Task<Scores> =
+    task {
+        let! scores = addScore (name, score)
+        return highScores scores
+    }
 
 let counterApi = {
-    initialCounter = getInitCounter >> Async.AwaitTask
+    getHighScores = getHighScores >> Async.AwaitTask
+    submitHighScore = submitHighScore >> Async.AwaitTask
 }
+
+let errorHandler (ex: Exception) (routeInfo: RouteInfo<_>) =
+    printfn "Error at %s on method %s: %s\n%s" routeInfo.path routeInfo.methodName ex.Message ex.StackTrace
+    Propagate ex
 
 let webApp =
     Remoting.createApi()
     |> Remoting.withRouteBuilder Route.builder
     |> Remoting.fromValue counterApi
+    |> Remoting.withErrorHandler errorHandler
     |> Remoting.buildHttpHandler
 
 let configureAzure (services:IServiceCollection) =
--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,13 +1,15 @@
 namespace Shared
 
-type Counter = int
-
 module Route =
-    /// Defines how routes are generated on server and mapped from client
     let builder typeName methodName =
         sprintf "/api/%s/%s" typeName methodName
 
-/// A type that specifies the communication protocol between client and server
-/// to learn more, read the docs at https://zaid-ajaj.github.io/Fable.Remoting/src/basics.html
-type ICounterApi =
-    { initialCounter : unit -> Async<Counter> }
+type Score = string * int
+type Scores = Score list
+
+module HighScores =
+    let limit = 10
+
+type IGameApi =
+    { getHighScores : unit -> Async<Scores>
+      submitHighScore : Score -> Async<Scores> }
