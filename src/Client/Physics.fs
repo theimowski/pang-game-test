f68b166f6bf93f6f594a1f12d79a30aa86c23688
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-23 15:52:41 +0200

    Higher buttons and add Physics.onCollisionWith

--- src/Client/Physics.fs
+++ src/Client/Physics.fs
@@ -94,3 +94,8 @@ let update (engine: Matter.Engine) (delta: float): unit =
 
 let moveVert (body: Matter.Body) (x : float) =
     matter.Body.applyForce(body, body.position, vector x 0.)
+
+let onCollisionWith (engine: Matter.Engine) handler =
+    matter.Events.on_collisionStart(engine, fun ev ->
+        for pair in ev.pairs do
+            handler pair)
